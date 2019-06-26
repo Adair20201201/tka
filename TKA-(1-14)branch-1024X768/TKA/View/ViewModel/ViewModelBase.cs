a3755e7cde6f49f3fe810b82e36ec094c70d5790
lyqsuperman <lyqsuperman@qq.com>
2019-06-26 17:08:17 +0800

    加入版本控制

---
+++ TKA-(1-14)branch-1024X768/TKA/View/ViewModel/ViewModelBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace TKA.ViewModel
+{
+    public class ViewModelBase:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> action)
+        {
+            var propertyName = GetPropertyName(action);
+            RaisePropertyChanged(propertyName);
+        }
+
+        private static string GetPropertyName<T>(Expression<Func<T>> action)
+        {
+            var expression = (MemberExpression)action.Body;
+            var propertyName = expression.Member.Name;
+            return propertyName;
+        }
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
