f28a95b250f64828b04bc647c8a2f2633b561bd4
lyq66 <lyqsuperman@qq.com>
2020-05-05 12:43:47 +0800

    最新

---
+++ TKA-(3-14)石家庄一场v1.3/TKA/Model/Channel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TKA.Model
+{
+    public class Channel
+    {
+        public string Setting { get; set; }
+
+        public int ChannelID { get; set; }
+    }
+}
